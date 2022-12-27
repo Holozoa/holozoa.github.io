a8cc5a3420832245a2f32e4615281e0e7b7ad179
Holozoa <32681354+Holozoa@users.noreply.github.com>
2022-12-27 06:07:23 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,23 +139,6 @@
   }
 
   /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-carousel', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
